cd8f71226ab809a01935ae626ac49eedb000d1a2
Mewnot <72050891+Mewnot-jar@users.noreply.github.com>
2022-05-23 17:17:21 -0400

    Add files via upload

---
+++ Problema1/Problema1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inicio = 0;
+            int final = 0;
+            int aux = 0;
+            Console.Write("Ingrese el inicio: ");
+            inicio = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese el final: ");
+            final = Convert.ToInt32(Console.ReadLine());
+
+            if (inicio < final)
+            {
+                aux = inicio;
+                inicio = final;
+                final = aux;
+            }
+
+            for (int i = inicio; i >= final; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
